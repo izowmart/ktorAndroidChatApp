32a982bf058f441d72c092e651e840aeba7fcd00
izowmart <izowmart8@gmail.com>
2022-02-09 08:41:03 +0300

    First commit to our android app

---
+++ app/src/main/java/com/example/ktor_chatappclient/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.ktor_chatappclient
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ktor_chatappclient.ui.theme.KtorchatAppClientTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KtorchatAppClientTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    KtorchatAppClientTheme {
+        Greeting("Android")
+    }
+}
